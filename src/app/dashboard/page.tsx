9ee5e585ae23e5e91af3591b443f8793a7c3ee70
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2025-01-24 01:25:26 -0300

    feat: páginas de registro e login de usuário e sessão mantida por cookies

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Dashboard(){
+    return(
+        <div>
+            <h1>Página Painel</h1>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,48 @@ import styles from './page.module.scss';
 import logoImg from '/public/logo.svg'
 import Image from 'next/image';
 import Link from 'next/link';
+import { api } from '@/services/api';
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 
 export default function Page(){
+
+  async function handleLogin(formData: FormData){
+    "use server"
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    if(email === '' || password === ''){
+      return
+    }
+
+    try {
+      const response = await api.post('/session', {
+        email,
+        password
+      })
+
+      if(!response.data.token){
+        return
+      }
+
+      // calculo de tempo de expiração do cookie (doc next)
+      const expressTime = 60 * 60 * 24 * 30 * 1000;
+
+      cookies().set("session", response.data.token, {
+        maxAge: expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production"
+      })
+
+    } catch (error) {
+      console.log('Error: ', error)
+    }
+
+    redirect('/dashboard')
+  }
 
   return(
     <>
@@ -15,7 +55,7 @@ export default function Page(){
 
         <section className={styles.login}>
           
-          <form>
+          <form action={handleLogin}>
               <input 
                 type="email" 
                 required
